f1210f9b90040827784023737cf5b8447460f41d
Corise <contact@thecorise.com>
2025-05-18 00:29:42 +0500

    Websocket Stable v1.0

--- config/config.js
+++ config/config.js
@@ -1,8 +1,7 @@
 module.exports = {
-    jwtSecret: process.env.JWT_SECRET,
-    jwtExpire: process.env.JWT_EXPIRE,
-    emailVerificationExpire: '24h',
-    passwordResetExpire: '1h',
-    aesSecretKey: process.env.AES_SECRET_KEY
-  };
-  
+  jwtSecret: process.env.JWT_SECRET || "archixol_user_backend",
+  jwtExpire: process.env.JWT_EXPIRE || "7d",
+  emailVerificationExpire: "24h",
+  passwordResetExpire: "1h",
+  aesSecretKey: process.env.AES_SECRET_KEY || "1234567890123456",
+};
